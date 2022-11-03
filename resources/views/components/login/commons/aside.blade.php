fcb8129db69a38a8429a8cdee6d6f2e8c9c49947
Gwenbatta0704 <gwenaellebatta@gmail.com>
2022-11-03 09:59:24 +0100

    test

--- resources/views/components/login/commons/aside.blade.php
+++ resources/views/components/login/commons/aside.blade.php
@@ -1,4 +1,5 @@
 <div class="flex flex-col mt-16 col-span-2">
+
     <figure class="grid grid-rows-3 grid-cols-2 gap-8 ">
         <img class="rounded-3xl row-start-3 col-start-1 col-end-4  order-4" src="/img-redimensions/brooke-cagle-g1Kr4Ozfoac-unsplash.jpg" alt="">
         <img class="rounded-3xl order-2" src="/img-redimensions/christopher-gower-m_HRfLhgABo-unsplash.jpg" alt="">
