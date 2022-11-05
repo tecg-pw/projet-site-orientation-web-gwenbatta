0706aee836cbcde7bad50099df1aa2dfeebb0c6d
Gwenbatta0704 <gwenaellebatta@gmail.com>
2022-11-05 11:37:56 +0100

    add arrow

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,10 +1,18 @@
 <article {{ $attributes->class(['flex-col flex bg-yellow-100 rounded-3xl relative']) }} aria-labelledby="slug">
-                <span class="flex-col flex px-4 mt-6">
-                    <h3 class="font-sans text-3xl mb-3" role="heading" aria-level="3" id="slug">{{__('Titre')}}</h3>
-                    <a href="user/single.blade.php">{{__('Name')}}</a>
-                    <p class="text-xl">Date du projet</p>
-                    <a class="self-end linkcard" href="/project/single">{{__('Voir le projet')}}</a>
-                </span>
+    <div class="flex-col flex px-4 mt-6">
+        <h3 class="font-sans text-3xl mb-3" role="heading" aria-level="3" id="slug">{{__('Titre')}}</h3>
+        <a href="user/single.blade.php">{{__('Name')}}</a>
+        <div class="flex justify-between mb-4">
+            <p class="text-xl">Date du projet</p>
+            <svg class="self-end " xmlns="http://www.w3.org/2000/svg" width="32" height="27.417"
+                 viewBox="0 0 32 27.417">
+                <path id="e6c07a72bd494f72e4a4d962e39f725c"
+                      d="M51,21.715a1.956,1.956,0,0,1-.56,1.355l-.012.023-11.75,11.75a1.958,1.958,0,1,1-2.769-2.769l8.405-8.409H20.958a1.958,1.958,0,0,1,0-3.917H44.314l-8.405-8.405a1.958,1.958,0,1,1,2.769-2.769l11.75,11.75.012.02A1.968,1.968,0,0,1,51,21.707Z"
+                      transform="translate(-19 -8.001)" fill="#4e6458"/>
+            </svg>
+        </div>
+        <a class="self-end linkcard" href="/project/single">{{__('Voir le projet')}}</a>
+    </div>
     <figure class="order-first">
         <img class="rounded-t-3xl" src="https://placehold.jp/526x526.png" alt="">
     </figure>
