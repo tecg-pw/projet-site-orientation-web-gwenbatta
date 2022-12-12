cf2728f27ea7215bb9dbe84388d7cd9b301d1808
Gwenbatta0704 <gwenaellebatta@gmail.com>
2022-12-12 14:29:36 +0100

    added overflow hidden when burger menu is opened

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,3 +1,13 @@
 import './bootstrap';
 
-console.log('test')
+let checkbox = document.getElementById('burger') as HTMLInputElement;
+let body = document.body as HTMLBodyElement;
+
+checkbox.addEventListener('change',(e)=>{
+    // @ts-ignore
+    if (e.currentTarget.checked){
+        body.classList.add('overflow-hidden')
+    }else{
+        body.classList.remove('overflow-hidden')
+    }
+});
