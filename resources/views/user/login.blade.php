593f68ae791d2d2305f1c728713939bc32cd03e4
Gwenbatta0704 <gwenaellebatta@gmail.com>
2023-01-03 11:58:24 +0100

    changes checkbox in login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,9 +1,11 @@
 <x-commons.navigation :page="__('login_register.login_title')"></x-commons.navigation>
 <main id="content">
-    <section class=" px-10 lg:px-16 xl:px-32 2xl:px-48 py-20 items-center xl:gap-24 flex justify-center" aria-labelledby="login">
+    <section class=" px-10 lg:px-16 xl:px-32 2xl:px-48 py-20 items-center xl:gap-24 flex justify-center"
+             aria-labelledby="login">
         <div class=" px-10">
             <div class="flex flex-col">
-                <h2 class="text-2xl md:text-3xl xl:text-4xl uppercase font-bold text-yellow-800 xl:mb-7 mb-3 font-sans order-1 mt-6" role="heading" aria-level="2" id="login">
+                <h2 class="text-2xl md:text-3xl xl:text-4xl uppercase font-bold text-yellow-800 xl:mb-7 mb-3 font-sans order-1 mt-6"
+                    role="heading" aria-level="2" id="login">
                     {{__('login_register.login_title')}}
                 </h2>
             </div>
@@ -15,35 +17,42 @@
                 <form action="/{{app()->getLocale()}}/login" method="post" class="flex flex-col xl:block min-w-full">
                     @csrf
                     <div class="flex flex-col mb-8">
-                        <label class="text-green-500 @error('email') text-red-400 @enderror text-lg xl:text-2xl" for="email">{{__('login_register.mail')}}</label>
+                        <label class="text-green-500 @error('email') text-red-400 @enderror text-lg xl:text-2xl"
+                               for="email">{{__('login_register.mail')}}</label>
                         @error('email')
                         <div class="flex gap-1.5 items-center">
-                            <svg class="h-7 w-7 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                            <svg class="h-7 w-7 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                             </svg>
                             <p class="text-red-500 text-lg font-semibold mt-2">{{ $message }}</p>
                         </div>
                         @enderror
                         <input
                             class="border border-orange-500 rounded-xl py-2 px-3 text-gray-700 leading-tight @error('email') outline-red-600 @enderror focus:outline-3 focus:outline-green-700 border focus:bg-orange-100"
-                            name="email" dusk="email-field" id="email" type="email" placeholder="email@example.be" value="{{old('email')}}">
+                            name="email" dusk="email-field" id="email" type="email" placeholder="email@example.be"
+                            value="{{old('email')}}">
                     </div>
                     <div class="flex flex-col mb-8">
-                        <label class="text-green-500 @error('password') text-red-400 @enderror text-lg xl:text-2xl" for="password">
+                        <label class="text-green-500 @error('password') text-red-400 @enderror text-lg xl:text-2xl"
+                               for="password">
                             {{__('login_register.password')}}
                         </label>
                         @error('password')
                         <div class="flex gap-1.5 items-center">
-                            <svg class="h-7 w-7 text-red-500"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                            <svg class="h-7 w-7 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
                             </svg>
                             <p class="text-red-500 text-lg font-semibold mt-2">{{ $message }}</p>
                         </div>
                         @enderror
-                        <div class="flex-1 px-3 items-center  @error('password') outline-red-600 @enderror flex border leading-tight border-orange-500 rounded-xl focus-within:border focus-within:border-2 focus-within:border-green-700 focus-within:bg-orange-100">
-                        <input
-                            class="password outline-none font-mono focus:bg-orange-100 py-2 text-gray-700 h-full w-full "
-                            name="password" dusk="password-field" id="password" type="password" placeholder="azerty">
+                        <div
+                            class="flex-1 px-3 items-center  @error('password') outline-red-600 @enderror flex border leading-tight border-orange-500 rounded-xl focus-within:border focus-within:border-2 focus-within:border-green-700 focus-within:bg-orange-100">
+                            <input
+                                class="password outline-none font-mono focus:bg-orange-100 py-2 text-gray-700 h-full w-full "
+                                name="password" dusk="password-field" id="password" type="password"
+                                placeholder="azerty">
                             <span class="show-password cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" width="20" height="20"
                              class="fill-orange">
@@ -57,15 +66,46 @@
                         <a class="hover:text-orange-500 mt-2 text-green-700 underline font-sans self-end text-sm"
                            href="/{{app()->getLocale()}}/user/password">{{__('login_register.forgot')}}</a>
                     </div>
-                    <div class="flex flex-row-reverse mb-8 justify-end gap-4">
-                        <label class="text-green-500 text-lg xl:text-2xl mr-4" for="remember">{{__('login_register.remind')}}</label>
-                        <input
-                            class="border border-orange-500 rounded-xl py-2 px-3 text-gray-700 leading-tight focus:outline-3 focus:outline-green-700 border focus:bg-orange-100"
-                            name="remember" id="remember" type="checkbox">
+                    <div class="flex mb-8 items-center gap-3">
+                        <input class="checkbox mb-1 absolute cursor-pointer opacity-0 z-30 w-6 h-6" type="checkbox" @if(old('softwares'))@checked(in_array($language->translation->where('locale',app()->getLocale(),)->first()->slug,old('languages')))@endif
+                               name="remember"
+                               id="remember">
+                                                    <span class="notCheck">
+                                                    <svg data-name="Composant 7 – 1"
+                                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                         viewBox="0 0 24 24">
+                                                      <g data-name="Rectangle 275" fill="#fff" stroke="#707070" stroke-width="1">
+                                                        <rect width="24" height="24" rx="6" stroke="none"/>
+                                                        <rect x="0.5" y="0.5" width="23" height="23" rx="5.5" fill="none"/>
+                                                      </g>
+                                                      <path id="d62ff70270774c6e6e679f7a8c643f83"
+                                                            d="M11.852,17.3a1.2,1.2,0,0,0,1.7,0l4.9-4.9a1.2,1.2,0,1,0-1.7-1.7L12.7,14.756l-1.452-1.464a1.2,1.2,0,0,0-1.7,1.7ZM24.8,2H3.2A1.2,1.2,0,0,0,2,3.2V24.8A1.2,1.2,0,0,0,3.2,26H24.8A1.2,1.2,0,0,0,26,24.8V3.2A1.2,1.2,0,0,0,24.8,2ZM23.6,23.6H4.4V4.4H23.6Z"
+                                                            transform="translate(-2 -2)" fill="#da953a"/>
+                                                      <path id="d62ff70270774c6e6e679f7a8c643f83-2" data-name="d62ff70270774c6e6e679f7a8c643f83"
+                                                            d="M11.852,17.3a1.2,1.2,0,0,0,1.7,0l4.9-4.9a1.2,1.2,0,1,0-1.7-1.7L12.7,14.756l-1.452-1.464a1.2,1.2,0,0,0-1.7,1.7ZM24.8,2H3.2A1.2,1.2,0,0,0,2,3.2V24.8A1.2,1.2,0,0,0,3.2,26H24.8A1.2,1.2,0,0,0,26,24.8V3.2A1.2,1.2,0,0,0,24.8,2ZM23.6,23.6H4.4V4.4H23.6Z"
+                                                            transform="translate(-2 -2)" fill="#da953a"/>
+                                                    </svg>
+                                                </span>
+                                                <span class="isCheck">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25">
+                                                      <g data-name="Composant 7 – 2" transform="translate(0.5 0.5)">
+                                                        <g data-name="Rectangle 275" transform="translate(0 0)" fill="#da953a" stroke="#da953a" stroke-width="1">
+                                                          <rect width="24" height="24" rx="6" stroke="none"/>
+                                                          <rect x="0.5" y="0.5" width="23" height="23" rx="5.5" fill="none"/>
+                                                        </g>
+                                                        <path d="M11.852,17.3a1.2,1.2,0,0,0,1.7,0l4.9-4.9a1.2,1.2,0,1,0-1.7-1.7L12.7,14.756l-1.452-1.464a1.2,1.2,0,0,0-1.7,1.7ZM24.8,2H3.2A1.2,1.2,0,0,0,2,3.2V24.8A1.2,1.2,0,0,0,3.2,26H24.8A1.2,1.2,0,0,0,26,24.8V3.2A1.2,1.2,0,0,0,24.8,2ZM23.6,23.6H4.4V4.4H23.6Z" transform="translate(-2 -2)" fill="#da953a" stroke="#da953a" stroke-width="1"/>
+                                                        <path data-name="d62ff70270774c6e6e679f7a8c643f83" d="M11.852,17.3a1.2,1.2,0,0,0,1.7,0l4.9-4.9a1.2,1.2,0,1,0-1.7-1.7L12.7,14.756l-1.452-1.464a1.2,1.2,0,0,0-1.7,1.7ZM24.8,2H3.2A1.2,1.2,0,0,0,2,3.2V24.8A1.2,1.2,0,0,0,3.2,26H24.8A1.2,1.2,0,0,0,26,24.8V3.2A1.2,1.2,0,0,0,24.8,2ZM23.6,23.6H4.4V4.4H23.6Z" transform="translate(-2 -2)" fill="#fff" stroke="#da953a" stroke-width="1"/>
+                                                      </g>
+                                                    </svg>
+                                                </span>
+                        <label class="mt-1 text-green-500 text-lg xl:text-2xl"
+                               for="remember">{{__('login_register.remind')}}</label>
+
                     </div>
                     <div class="flex flex-1 justify-end mt-10  xl:mt-0">
-                        <button class="hover:text-green-700 hover:bg-white-100 text-center rounded-lg px-4 py-2 mb-3 text-white-100 bg-green-700 font-sans font-semibold md:text-xl xl:border-2 xl:border-green-700 xl:mb-0 xl:text-center xl:px-10 xl:py-3 xl:rounded-2xl xl:text-2xl flex-1 xl:flex-initial"
-                                type="submit" dusk="submit-credentials">{{__('login_register.login_button')}}</button>
+                        <button
+                            class="hover:text-green-700 hover:bg-white-100 text-center rounded-lg px-4 py-2 mb-3 text-white-100 bg-green-700 font-sans font-semibold md:text-xl xl:border-2 xl:border-green-700 xl:mb-0 xl:text-center xl:px-10 xl:py-3 xl:rounded-2xl xl:text-2xl flex-1 xl:flex-initial"
+                            type="submit" dusk="submit-credentials">{{__('login_register.login_button')}}</button>
                     </div>
                 </form>
             </div>
